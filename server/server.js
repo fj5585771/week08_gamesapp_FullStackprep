fd455886ba287c7dc2b04f8f3a8c053992bc4886
fraser jamieson <fraserjamieson.93@gmail.com>
2021-01-06 10:56:25 +0000

    updated answeres after recap session w/ niall

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const app = express();
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
-const cors = require('cors');
+const cors = require('cors'); // <-- allows only requests from resources we know/trust
 
-app.use(cors())
-app.use(parser.json())
+app.use(cors()) // use "cross origin resource sharing"
+app.use(parser.json()) 
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   if(err){
@@ -15,7 +15,7 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
 
   const db = client.db('games_hub');
   const gamesCollection = db.collection('games');
-  const gamesRouter = createRouter(gamesCollection)
+  const gamesRouter = createRouter(gamesCollection)  // <-- handles CRUD actions
   app.use('/api/games', gamesRouter);
 
   app.listen(5000, function(){
